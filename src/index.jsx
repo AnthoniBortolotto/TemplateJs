7e5ca004f28f9eba5cf8823f4bfd1c232dc70734
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-05-14 15:07:04 -0300

    ajustes necessarios feitos

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter, Switch, Route,
+} from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import Home from './pages/Home';
+import Cabecario from './organisms/Cabecario';
+import About from './pages/About';
+import Buy from './pages/Buy';
+import './atoms/css/animate.min.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#616161',
+    },
+    secondary: {
+      main: '#d50000',
+    },
+  },
+});
+const App = () => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Cabecario />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/aboutus" component={About} />
+        <Route path="/buy" component={Buy} />
+      </Switch>
+    </ThemeProvider>
+  </BrowserRouter>
+);
+ReactDOM.render(<App />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,67 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname,'./src/index.js'),
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-        chunkFilename: '[id].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)x?$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+              sourceMap: true,
             },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'style-loader' },
-                    { 
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: "[name]__[local]___[hash:base64:5]",
-                            },														
-                            sourceMap: true
-                        }
-                     },
-                     { 
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [ 'autoprefixer', {}, ],
-                                ],
-                            },
-                        }
-                      }
-                ]
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['autoprefixer', {}],
+                ],
+              },
             },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/templates/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=10000&name=img/[name].[ext]',
+      },
     ],
-    devServer: {
-        port: 8080,
-        contentBase: path.resolve(__dirname, './dist'),
-        compress: true,
-        historyApiFallback: true,
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/templates/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    historyApiFallback: true,
+  },
 };
